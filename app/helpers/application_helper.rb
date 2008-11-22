10620c05b70194d35ada6e63f69e52017431c495
Matt Hooks <matthooks@gmail.com>
2008-11-21 19:09:01 -0600

    removed prototype, added flash message helper, added simple app layout, updated styles and included common rails error styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
-# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  # Sensible flash messages
+  def flash_helper(tag = :p)
+    flash.collect do |fl|
+      content_tag(tag, fl.last, :class => "#{fl.first} flash")
+    end
+  end
+  
 end
