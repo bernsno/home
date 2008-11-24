3271b94098386af8c7538b831466f4b754933ea0
Matt Hooks <matthooks@gmail.com>
2008-11-24 04:55:38 -0600

    fixed signup, added some convenienve routes, added jquery

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resources :password_resets
 
+  map.logout "/logout", :controller => 'user_sessions', :action => 'destroy'
+  map.login '/login', :controller => 'user_sessions', :action => 'new'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+
   map.resource :user_session
   map.root :controller => "user_sessions", :action => "new"
 end
