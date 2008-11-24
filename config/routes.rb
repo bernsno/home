2723c61647566752469ac09c82115bae70a0908f
Matt Hooks <matthooks@gmail.com>
2008-11-24 05:01:23 -0600

    for some reason double quotes bother me in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resource :account, :controller => "users"
+  map.resource :account, :controller => 'users'
   map.resources :users
   
   map.resources :password_resets
 
-  map.logout "/logout", :controller => 'user_sessions', :action => 'destroy'
+  map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
   map.login '/login', :controller => 'user_sessions', :action => 'new'
   map.signup '/signup', :controller => 'users', :action => 'new'
 
   map.resource :user_session
-  map.root :controller => "user_sessions", :action => "new"
+  map.root :controller => 'user_sessions', :action => 'new'
 end
