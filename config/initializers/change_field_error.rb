1395c34f3c8beadc67e8c9477d89f1da34f93c7c
Noah Bernsohn <noah@noah-bernsohns-macbook.local>
2009-01-13 11:14:08 -0600

    added change field error

---
+++ config/initializers/change_field_error.rb
@@ -0,0 +1,8 @@
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+  if html_tag =~ /type="hidden"/ || html_tag =~ /<label/
+    html_tag
+  else
+    "<span class='field_error'>#{html_tag}</span>"
+  end
+end
+# Thanks Ryan Bates!
