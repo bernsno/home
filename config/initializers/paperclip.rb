6b7deeec6edfe2842941395ad09b565de0ce4b34
Matt Hooks <matthooks@gmail.com>
2009-01-15 20:00:54 -0600

    updated paperclip init to avoid deprec msg

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,9 +2,9 @@ identify_dir = File.dirname(%x[which convert])
 convert_dir = File.dirname(%x[which identify])
 
 if (Rails.env.development? || Rails.env.test?)
-  Paperclip.options[:image_magick_path] = '/usr/local/bin/'
+  Paperclip.options[:command_path] = '/usr/local/bin/'
 elsif !(identify_dir.blank? && convert_dir.blank?) && (identify_dir == convert_dir)
-  Paperclip.options[:image_magick_path] = identify_dir
+  Paperclip.options[:command_path] = identify_dir
 else
-  Paperclip.options[:image_magick_path] = '/usr/bin/'
+  Paperclip.options[:command_path] = '/usr/bin/'
 end
