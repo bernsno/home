324ee40083fd25e193751cf119af0b7fa6674b80
Matt Hooks <matthooks@gmail.com>
2009-01-16 03:38:06 -0600

    added google analytics partial, minor tweaks to app layout, todo of user controller tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,13 @@ module ApplicationHelper
   end
   
   def title
-    title = Settings.site_name
-    title += "- #{@title}" if @title
+    returning title = Settings.site_name do
+      title += " - #{@title}" if @title
+    end
+  end
+  
+  def auth_token
+    javascript_tag "var AUTH_TOKEN = #{form_authenticity_token.inspect};" if protect_against_forgery?
   end
   
   def body_class
