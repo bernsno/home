060fb0632029ace523c50acd9f480dcc7b82f9ff
Noah Bernsohn <noah@noah-bernsohns-macbook.local>
2009-03-09 22:21:38 -0500

    Add generated session configuration. (Eden)

---
+++ config/session.rb
@@ -0,0 +1,8 @@
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+SESSION_CONFIG = {
+  :session_key => '_home_session',
+  :secret      => 'f45f6b8b9863820ca395139e8ba1c3969f9fbbd4b8789062d61c6a71895ef7ef86d27b5d57351ec5f74379676cd4f4910c728fa17719768abc23add25e323866f5f93f3e33b33821bbf58d66fb3484ee4dd52f8d1ce64455ecced7f31ff6a01bdd75bdfce1e7d2b8f0c9c5b6038dcb0d414e8af2e18ab631e1074155158182b1127d'
+}.freeze
