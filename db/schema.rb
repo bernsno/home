7dae5f97d1b9ce9078279d3947e5bfb560d477c3
Noah Bernsohn <noah@noah-bernsohns-macbook.local>
2009-03-10 19:12:35 -0500

    rebase update

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090223085229) do
+ActiveRecord::Schema.define(:version => 20090310045503) do
 
   create_table "accounts", :force => true do |t|
     t.string   "subdomain"
